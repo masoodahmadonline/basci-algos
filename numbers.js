55a4a8fe55459a47653673a77f4e78d701b77fec
masoodahmadonline <46044455+masoodahmadonline@users.noreply.github.com>
2020-06-11 11:20:34 +0300

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,13 +1,13 @@
 // Check if a number is a prime number /////////////////////////
 const isPrime = (n) => {
-	let divisor = 2;
+  let divisor = 2;
   while (n > divisor){
   console.log('divisor: ', divisor);
-  	if(n % divisor === 0) {
+    if(n % divisor === 0) {
     	return false;
     } else if (divisor !== 2){
     	divisor+= 2;
     } else divisor ++;
   }
-	return true;
+  return true;
 };
